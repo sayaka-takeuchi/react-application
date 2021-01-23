e963907d557c46ce2427c025eda9729bd31ee5d2
sayaka-takeuchi <ars0915.cute0910@gmail.com>
2021-01-23 16:46:57 +0900

    jsxについて学習

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-function App() {
-  return (
-    <div><h1>Hello,world!</h1></div>
-  );
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => console.log("Hello!")} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
